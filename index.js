7ff022a3b0e54aaf376c54176bc3416175f0862b
alu0100713213 <alu0100713213@ull.edu.es>
2015-11-06 11:09:14 +0000

    Solucionando iaas

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
 var path = require('path');
+
+var temperatura = require("static/temperatura.js");
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); // http://expressjs.com/api.html#app.set
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 var expressLayouts = require('express-ejs-layouts');
-app.set('layout', 'layout'); // defaults to 'layout' '
-app.use(express.static('.')); // http://expressjs.com/api.html#app.use#
+app.set('layout', 'layout'); // defaults to 'layout'  '
+
+// Serve static files
+app.use(express.static('.'));
 app.use(expressLayouts);
+
 app.set('port', (process.env.PORT || 8080));
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
-	res.render('body', { title: "form"});
+  res.render('index');
 });
 
 app.post('/', function(req, res){
-	var temp = new Temperatura();
-	var respuesta = temp.convertir(req.body.original);
-	res.render('pages/index',{title:"Conversor de Temperatura ExpressJS", result: respuesta});
+  var temp = new temperatura();
+
+  var resultado = temp.convertir(req.body.original);
+   res.render('res', {resultado: resultado, title: 'res'});
 });
+
 app.listen(app.get('port'), function() {
-console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-
